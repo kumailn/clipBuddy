244ee8631db1731cd7ac1a0e53ee7923f6d0dc37
Kumail <kumailmn@gmail.com>
2017-09-16 09:38:27 -0400

    bubble

--- app/src/main/java/com/kumailn/testapp/MainActivity.java
+++ app/src/main/java/com/kumailn/testapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.kumailn.testapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         Button myButton = (Button)findViewById(R.id.button);
         Button b2 = (Button)findViewById(R.id.button2);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+
+            //If the draw over permission is not available open the settings screen
+            //to grant the permission.
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivity(intent);
+        }
+
+
         myButton.setText("Service One");
         myButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -27,8 +40,13 @@ public class MainActivity extends AppCompatActivity {
         myButton.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                Intent i = new Intent(getApplicationContext(), MyService.class);
-                stopService(i);
+                /*Intent i = new Intent(getApplicationContext(), MyService.class);
+                stopService(i);*/
+
+
+
+                startService(new Intent(MainActivity.this, ChatHeadService.class));
+                finish();
                 return true;
             }
         });

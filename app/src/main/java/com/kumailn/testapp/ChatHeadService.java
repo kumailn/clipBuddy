ee6a2920d6df9c6eddd66f52a0a2548f750a76fa
asadmansr <asadmansr@gmail.com>
2017-09-16 22:30:33 -0400

    Added intent and images to ui bubble

--- app/src/main/java/com/kumailn/testapp/ChatHeadService.java
+++ app/src/main/java/com/kumailn/testapp/ChatHeadService.java
@@ -1,10 +1,12 @@
 package com.kumailn.testapp;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.IBinder;
+import android.provider.ContactsContract;
 import android.support.annotation.IntDef;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -14,6 +16,7 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -34,6 +37,13 @@ public class ChatHeadService extends Service {
     private boolean isActivited = false;
     TextView primaryTV;
     TextView secondaryTV;
+    ImageView firstTabIV;
+    ImageView secondTabIV;
+    ImageView thirdTabIV;
+    RelativeLayout relativetabLayout;
+    RelativeLayout relativeLayout;
+    LinearLayout buttonLL;
+    RelativeLayout closeButton;
 
 
     public ChatHeadService() {
@@ -50,6 +60,7 @@ public class ChatHeadService extends Service {
         super.onCreate();
         mChatHeadView = LayoutInflater.from(this).inflate(R.layout.chat_head, null);
 
+
         final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.MATCH_PARENT,
@@ -59,16 +70,21 @@ public class ChatHeadService extends Service {
 
         params.gravity = Gravity.TOP | Gravity.RIGHT;
         params.x = 0;
-        params.y = 100;
+        params.y = 0;
 
         mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         mWindowManager.addView(mChatHeadView, params);
 
-        RelativeLayout relativeLayout = (RelativeLayout) mChatHeadView.findViewById(R.id.container_rl);
+        relativeLayout = (RelativeLayout) mChatHeadView.findViewById(R.id.container_rl);
         primaryTV = (TextView) mChatHeadView.findViewById(R.id.primary_tv);
         secondaryTV = (TextView) mChatHeadView.findViewById(R.id.secondary_tv);
-        LinearLayout buttonLL = (LinearLayout) mChatHeadView.findViewById(R.id.button_ll);
-        RelativeLayout closeButton = (RelativeLayout) mChatHeadView.findViewById(R.id.close_rl);
+        firstTabIV = (ImageView) mChatHeadView.findViewById(R.id.tab1);
+        secondTabIV = (ImageView) mChatHeadView.findViewById(R.id.tab2);
+        thirdTabIV = (ImageView) mChatHeadView.findViewById(R.id.tab3);
+        relativetabLayout = (RelativeLayout) mChatHeadView.findViewById(R.id.tab3_rl);
+
+        buttonLL = (LinearLayout) mChatHeadView.findViewById(R.id.button_ll);
+        closeButton = (RelativeLayout) mChatHeadView.findViewById(R.id.close_rl);
         final RelativeLayout chatHeadImage = (RelativeLayout) mChatHeadView.findViewById(R.id.icon_rl);
 
         Animation floatingButton = AnimationUtils.loadAnimation(getBaseContext(), R.anim.animation_open_button);
@@ -126,6 +142,7 @@ public class ChatHeadService extends Service {
                             secondaryTV.setVisibility(View.VISIBLE);
                             buttonLL.setVisibility(View.VISIBLE);
                             relativeLayout.setVisibility(View.VISIBLE);
+                            closeButton.setVisibility(View.VISIBLE);
 
                             Animation expandIn = AnimationUtils.loadAnimation(getBaseContext(), R.anim.animation);
                             relativeLayout.startAnimation(expandIn);
@@ -136,6 +153,7 @@ public class ChatHeadService extends Service {
                             secondaryTV.setVisibility(View.GONE);
                             buttonLL.setVisibility(View.GONE);
                             relativeLayout.setVisibility(View.GONE);
+                            closeButton.setVisibility(View.GONE);
                         }
 
                         isActivited = !isActivited;
@@ -162,14 +180,87 @@ public class ChatHeadService extends Service {
         String phone_number = intent.getStringExtra("PHONENUMBER");
 
         if (data_type.equals("PHONE")){
-            passPhoneIntent(phone_number);
-        }
-        else if(data_type.equals("EMAIL")){
-            passEmailIntent(email_address);
-        }
-        else if(data_type.equals("CURRENCY")){
-            primaryTV.setText(converted_value);
-            secondaryTV.setText(original_value);
+            primaryTV.setText(phone_number);
+            secondaryTV.setText("");
+            firstTabIV.setBackgroundResource(R.drawable.ic_phone_white_24dp);
+            secondTabIV.setBackgroundResource(R.drawable.ic_message_white_24dp);
+            thirdTabIV.setBackgroundResource(R.drawable.ic_person_white_24dp);
+            relativetabLayout.setBackgroundResource(R.drawable.button_tabs);
+
+            firstTabIV.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    passPhoneIntent(phone_number);
+
+                }
+            });
+
+            secondTabIV.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    passMessagesIntent(phone_number);
+                }
+            });
+
+            thirdTabIV.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    passContactPhoneIntent(phone_number);
+                }
+            });
+        }  else if(data_type.equals("EMAIL")){
+            primaryTV.setText(email_address);
+            secondaryTV.setText("");
+            firstTabIV.setBackgroundResource(R.drawable.ic_email_white_24dp);
+            secondTabIV.setBackgroundResource(R.drawable.ic_person_white_24dp);
+            thirdTabIV.setBackgroundResource(R.drawable.ic_phone_white_24dp);
+            thirdTabIV.setEnabled(false);
+            relativetabLayout.setBackgroundResource(R.drawable.buttons_tabs_disabled);
+
+            firstTabIV.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    passEmailIntent(email_address);
+
+                }
+            });
+
+            secondTabIV.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    passContactEmailIntent(email_address);
+                }
+            });
+
+        } else if(data_type.equals("CURRENCY")){
+            primaryTV.setText("CAD" + " " + converted_value);
+            secondaryTV.setText(currency_code+" "+ original_value);
+            firstTabIV.setBackgroundResource(R.drawable.ic_attach_money_white_24dp);
+            secondTabIV.setBackgroundResource(R.drawable.ic_euro_symbol_white_24dp);
+            thirdTabIV.setBackgroundResource(R.drawable.ic_euro_symbol_white_24dp);
+            relativetabLayout.setBackgroundResource(R.drawable.button_tabs);
+
+            firstTabIV.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+
+                }
+            });
+
+            secondTabIV.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+
+            thirdTabIV.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
         }
 
 
@@ -184,10 +275,38 @@ public class ChatHeadService extends Service {
         if (mChatHeadView != null) mWindowManager.removeView(mChatHeadView);
     }
 
+    public void passContactPhoneIntent(String phoneNumber){
+        Intent intent = new Intent(Intent.ACTION_INSERT);
+        intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+        intent.putExtra(ContactsContract.Intents.Insert.PHONE, phoneNumber);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        minimizeChatHead();
+    }
+
+    public void passContactEmailIntent(String email){
+        Intent intent = new Intent(Intent.ACTION_INSERT);
+        intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+        intent.putExtra(ContactsContract.Intents.Insert.EMAIL, email);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        minimizeChatHead();
+    }
+
+    public void passMessagesIntent(String phonenumber){
+        Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+        sendIntent.setData(Uri.parse("sms:"+phonenumber));
+        startActivity(sendIntent);
+        minimizeChatHead();
+    }
+
     public void passPhoneIntent(String phonenumber){
         Intent intent = new Intent(Intent.ACTION_DIAL);
         intent.setData(Uri.parse("tel:" + phonenumber));
-        startActivity(intent);
+        getBaseContext().startActivity(intent);
+        minimizeChatHead();
     }
 
     public void passEmailIntent(String emailAddress){
@@ -197,6 +316,21 @@ public class ChatHeadService extends Service {
         intent.putExtra(Intent.EXTRA_TEXT, "");
         intent.setData(Uri.parse("mailto:" + emailAddress)); // or just "mailto:" for blank
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // this will make such that when user returns to your app, your app is displayed, instead of the email app.
-        startActivity(intent);
+        getBaseContext().startActivity(intent);
+        minimizeChatHead();
     }
+
+    private void minimizeChatHead(){
+        Animation expandOut = AnimationUtils.loadAnimation(getBaseContext(), R.anim.animation_close);
+        relativeLayout.startAnimation(expandOut);
+        primaryTV.setVisibility(View.GONE);
+        secondaryTV.setVisibility(View.GONE);
+        buttonLL.setVisibility(View.GONE);
+        relativeLayout.setVisibility(View.GONE);
+        closeButton.setVisibility(View.GONE);
+
+        isActivited = false;
+
+    }
+
 }
